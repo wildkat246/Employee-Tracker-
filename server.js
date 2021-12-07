4ab1e2f45122083eaf1460573f19fc24a89ab990
wildkat246 <85767537+wildkat246@users.noreply.github.com>
2021-12-07 18:05:25 -0500

    changed server.js 2

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
+require('dotenv').config();
+const dotEnv = require("dotenv").config();
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-require('dotenv').config();
-const dotEnv = require("dotenv").config();
-
 
 const app = express();
 const PORT = process.env.PORT || 3306;
 
-//Creating connection 
-//const connection = mysql.createConnection({
-  //  host: 'localhost',
-    //user: 'root',
-   // database: 'test'
-  //});
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+});
 
 inquirer.prompt([
         {
@@ -91,13 +91,19 @@ inquirer.prompt([
     }
   });
 
-function viewAllDepartments() {
-    console.log("view departs");
-    //make sure database has seeds 
-    //query the database to find the employees
-    //use console.table to log variable for the departments
+  function viewAllDepartments() {
+    connection.query("SELECT department_table.id 'ID', department_table.department_name 'Department Name' FROM department_table", 
+    function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startUp();
+      });
 }
 
 function viewAllRoles() {
-    console.log("view roles");
+    connection.query(`SELECT role_table.id "ID", role_table.title "Title", role_table.salary "Salary", department_table.department_name "Department Name" FROM role_table JOIN department_table ON department_table.id = role_table.department_id`, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startUp();
+      });
 }
